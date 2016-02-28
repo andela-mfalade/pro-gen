698bebead6d0e353bdc9c46a065483b4d3cbba60
andela-mfalade <mayowa.falade@andela.com>
2016-02-28 12:14:11 +0100

    Include setup file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""setup script.
+
+Basic project setup script.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pro-gen',
+    version='0.0.1',
+    description='Random Profile generator',
+    license=None,
+    author='Falade Mayowa',
+    author_email='falademayowa240@gmail.com',
+    install_requires=[],
+    url='https://github.com/amandapickering/markovgenerator',
+    keywords='Profile Random names address generator job job-title city description',
+    packages=find_packages(),
+)
