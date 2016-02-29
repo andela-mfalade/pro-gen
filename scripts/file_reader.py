3d92b956fffd97de002bebc2fde50f6c2e233d93
andela-mfalade <mayowa.falade@andela.com>
2016-02-29 12:15:22 +0100

    initialize file reader script

---
+++ scripts/file_reader.py
@@ -0,0 +1,10 @@
+"""Read text files and extract contents.
+
+Read a given text file, parse and format its contents, then return the result
+"""
+
+
+
+def extract_file_contents(file_path):
+    with open(file_path, 'r') as f:
+        import ipdb; ipdb.set_trace()
