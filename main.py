ffb041f6bef0721958465c0d0eb36faaec4fe303
andela-mfalade <mayowa.falade@andela.com>
2016-03-09 13:46:11 +0100

    Modify markov.parse script

--- main.py
+++ main.py
@@ -2,19 +2,29 @@
 
 Main script to initiate biogenesis.
 """
+import codecs
 import time
 
+from markov.parse import Parser
 from scripts import bio_server
 
 
 server = bio_server.BioServer()
 
+SENTENCE_SEPARATOR = '.'
+WORD_SEPARATOR = ' '
+
 
 def main():
-    for i in range(50):
+    for i in range(5):
         bio = server.fetch_new_bio()
         print "Profile %d >> " % i, bio
         time.sleep(0.3)
+    file_path = 'resources/GrowingCity.txt'
+    txt = codecs.open(file_path, 'r', 'utf-8').read()
+    name = 'growing_city'
+    Parser(name, depth=8, ssc=SENTENCE_SEPARATOR, wsc=WORD_SEPARATOR).parse(txt)
+
 
 if __name__ == '__main__':
     main()
