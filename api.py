04490fa1df7199dc3007c69fe0ed147b0468af78
andela-mfalade <mayowa.falade@andela.com>
2016-03-13 09:48:09 +0000

    Set Root directory on production.. modify error response.

--- api.py
+++ api.py
@@ -1,7 +1,3 @@
-"""Progen entry point.
-
-Main script to initiate biogenesis.
-"""
 from flask import Flask
 from flask import jsonify
 from flask import make_response
@@ -19,12 +15,16 @@ def server_error(error):
         'success': False,
         'status_code': 404,
         'message': 'Nothing Exists On This Endpoint',
-        'valid_request': '/api/v1/profiles?count=<number of desired profile.>',
+        'valid_request_samples': {
+            'sample1': 'http://progen.pythonanywhere.com/api/v1/profile',
+            'sample2': 'http://progen.pythonanywhere.com/api/v1/profiles?count=<number of desired profile.>'
+        },
         'links': {
             'self': str(request.url)
-        },
+         },
     }
     return make_response(jsonify(error_message))
+
 
 
 @app.errorhandler(400)
@@ -33,7 +33,10 @@ def server_error(error):
         'success': False,
         'status_code': 400,
         'message': 'You are doing this wrong. You need to specify the count.',
-        'valid_request': '/api/v1/profiles?count=<number of desired profile.>',
+        'valid_request_samples': {
+            'sample1': 'http://progen.pythonanywhere.com/api/v1/profile',
+            'sample2': 'http://progen.pythonanywhere.com/api/v1/profiles?count=<number of desired profile.>'
+        },
         'links': {
             'self': str(request.url)
         },
@@ -41,29 +44,35 @@ def server_error(error):
     return make_response(jsonify(error_message))
 
 
+
+@app.route('/', methods=['GET'])
+def welcome_home():
+    return jsonify({'Text': 'Welcome to the Progen API.'})
+
+
 @app.route('/api/v1/profiles', methods=['GET'])
 def fetch_profiles():
-    response = {
-        'success': True,
-        'status_code': 200,
-        'links': {
-            'self': str(request.url)
-        },
-    }
-    profile_count = int(request.args['count'])
-    if profile_count > 20:
-        error_message = {
-            'success': False,
-            'status_code': 413,
-            'message': 'Request Limit Exceeded. 20 or less counts allowed per request.',
+        response = {
+            'success': True,
+            'status_code': 200,
             'links': {
                 'self': str(request.url)
             },
         }
-        return jsonify(error_message)
-    else:
-        response.update({'data':  executr.fetch_profile(profile_count)})
-        return jsonify(response)
+        profile_count = int(request.args['count'])
+        if profile_count > 20:
+            error_message = {
+                'success': False,
+                'status_code': 413,
+                'message': 'Request Limit Exceeded. 20 or less counts allowed per request.',
+                'links': {
+                    'self': str(request.url)
+                },
+            }
+            return jsonify(error_message)
+        else:
+            response.update({'data':  executr.fetch_profile(profile_count)})
+            return jsonify(response)
 
 
 @app.route('/api/v1/profile', methods=['GET'])
@@ -71,10 +80,11 @@ def fetch_single_profile():
     response = {
         'success': True,
         'status_code': 200,
-        'data': executr.fetch_profile(),
+        'data': executr.fetch_profile()
     }
     return jsonify(response)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
--- config.py
+++ config.py
@@ -3,6 +3,8 @@
 This exists to make file paths easily configurable within the program.
 """
 
+ROOT_DIR = '/home/progen/pro-gen/'
+
 
 valueof = {
     'LUCKY_NUM': 10,
@@ -15,23 +17,23 @@ valueof = {
 
 
 pathto = {
-    'THE_PLANET': 'resources/ThePlanet.txt',
-    'AFRICAN_NAMES': 'resources/African-Names.csv',
-    'GROWING_CITY': 'resources/GrowingCity.txt',
-    'PLANET_FRONDS': 'resources/PlanetFronds.txt',
-    'CITY_NEIGHBORHOOD': 'resources/TheCityNeighborhood.txt',
+    'THE_PLANET': ROOT_DIR + 'resources/ThePlanet.txt',
+    'AFRICAN_NAMES': ROOT_DIR + 'resources/African-Names.csv',
+    'GROWING_CITY': ROOT_DIR + 'resources/GrowingCity.txt',
+    'PLANET_FRONDS': ROOT_DIR + 'resources/PlanetFronds.txt',
+    'CITY_NEIGHBORHOOD': ROOT_DIR + 'resources/TheCityNeighborhood.txt',
 
 
-    'FRONDS_PLANET_FILE': 'resources/FrondsPlusPlanet.txt',
-    'GROWING_NEIGHBORHOOD_FILE': 'resources/GrowingPlusNeighborhood.txt',
+    'FRONDS_PLANET_FILE': ROOT_DIR + 'resources/FrondsPlusPlanet.txt',
+    'GROWING_NEIGHBORHOOD_FILE': ROOT_DIR + 'resources/GrowingPlusNeighborhood.txt',
 
 
-    'THE_PLANET_PROCESSED': 'results/ThePlanetProcessed.txt',
-    'GROWING_CITY_PROCESSED': 'results/GrowingCityProcessed.txt',
-    'PLANET_FRONDS_PROCESSED': 'results/PlanetFrondsProcessed.txt',
-    'CITY_NEIGHBORHOOD_PROCESSED': 'results/TheCityNeighborhoodProcessed.txt',
+    'THE_PLANET_PROCESSED': ROOT_DIR + 'results/ThePlanetProcessed.txt',
+    'GROWING_CITY_PROCESSED': ROOT_DIR + 'results/GrowingCityProcessed.txt',
+    'PLANET_FRONDS_PROCESSED': ROOT_DIR + 'results/PlanetFrondsProcessed.txt',
+    'CITY_NEIGHBORHOOD_PROCESSED': ROOT_DIR + 'results/TheCityNeighborhoodProcessed.txt',
 
 
-    'GROWING_CITY_NEIGHBORHOOD': 'results/GrowintCity-Plus-CityNeighborhood.txt',
-    'THE_PLANET_FRONDS': 'results/ThePlanet-Plus-PlanetFronds.txt',
+    'GROWING_CITY_NEIGHBORHOOD': ROOT_DIR + 'results/GrowintCity-Plus-CityNeighborhood.txt',
+    'THE_PLANET_FRONDS': ROOT_DIR + 'results/ThePlanet-Plus-PlanetFronds.txt',
 }
